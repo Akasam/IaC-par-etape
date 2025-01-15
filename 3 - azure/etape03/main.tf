27b1c3840d91fe6af677dadcbf8e9587febac836
akasam <srohaut@gmail.com>
2025-01-15 22:42:52 +0100

    [add] Azure Etape 3

---
+++ 3 - azure/etape03/main.tf
@@ -0,0 +1,60 @@
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name             = "internal"
+    address_prefixes = ["10.0.2.0/24"] # Note the brackets and plural form
+  }
+}
+# Create a simple Linux VM
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub") # Your SSH public key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest" # or "24.04.202412190" if you want this specific version
+  }
+}
+
+# Create a network interface
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_virtual_network.example.subnet.*.id[0]
+    private_ip_address_allocation = "Dynamic"
+  }
+}
---
+++ 3 - azure/etape03/provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  subscription_id = "28f20d9f-6191-4f3d-97f0-d63c1f45e004"
+  client_id       = "f5d86850-8eea-4abd-8457-fde4366a5730"
+  client_secret   = "1zJ8Q~bylRi4rG-XcR2LaU7arB6SCdiHlfEqMbwV"
+  tenant_id       = "b7b023b8-7c32-4c02-92a6-c8cdaa1d189c"
+  features {}
+}
