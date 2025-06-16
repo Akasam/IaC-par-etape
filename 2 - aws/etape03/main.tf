4a8450c26af55e9b213f5eea6a534211430c386d
akasam <srohaut@gmail.com>
2025-06-17 00:46:02 +0200

    [add] AWS Academy Lab Learner

--- 2 - aws/etape03/main.tf
+++ 2 - aws/etape03/main.tf
@@ -3,18 +3,20 @@
 #     --filters "Name=name,Values=*ubuntu*24.04*amd64-server-*" "Name=state,Values=available" \
 #     --query 'sort_by(Images, &CreationDate)[-1].[ImageId,Name]' \
 #     --output text \
-#     --region eu-west-3
+#     --region us-east-1
 # AMI : Amazon Machine Image
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-west-3"
+  region                   = "us-east-1"
+  shared_credentials_files = ["../_credentials/aws_learner_lab_credentials"]
+  profile                  = "awslearnerlab"
 }
 
 # Create an EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-0f209d0bb2c44ea6c"
-  key_name      = "myKey"
+  ami           = "ami-02f7c3a0c32f3f59e"
+  key_name      = "vockey"
   instance_type = "t2.micro"
 
   tags = {
