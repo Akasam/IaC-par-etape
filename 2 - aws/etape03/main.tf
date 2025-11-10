483b50e00ced63be600da674080bbaf078331a52
Akasam <srohaut@gmail.com>
2025-11-10 12:19:56 +0100

    Revert "[add] AWS Academy Lab Learner"
    This reverts commit 4a8450c26af55e9b213f5eea6a534211430c386d.

--- 2 - aws/etape03/main.tf
+++ 2 - aws/etape03/main.tf
@@ -3,20 +3,18 @@
 #     --filters "Name=name,Values=*ubuntu*24.04*amd64-server-*" "Name=state,Values=available" \
 #     --query 'sort_by(Images, &CreationDate)[-1].[ImageId,Name]' \
 #     --output text \
-#     --region us-east-1
+#     --region eu-west-3
 # AMI : Amazon Machine Image
 
 # Configure the AWS Provider
 provider "aws" {
-  region                   = "us-east-1"
-  shared_credentials_files = ["../_credentials/aws_learner_lab_credentials"]
-  profile                  = "awslearnerlab"
+  region = "eu-west-3"
 }
 
 # Create an EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-02f7c3a0c32f3f59e"
-  key_name      = "vockey"
+  ami           = "ami-0f209d0bb2c44ea6c"
+  key_name      = "myKey"
   instance_type = "t2.micro"
 
   tags = {
